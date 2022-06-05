e19c03bc4d3e8bc5b28b39e3286e34aca592d529
terlyga78 <telyga78@gmail.com>
2022-06-05 14:29:17 +0300

    end

---
+++ app.js
@@ -0,0 +1,106 @@
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
+
+const INITIAL_COLORS = '#2c2c2c';
+const CANVAS_SIZE = 700;
+
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
+
+// ctx.fillStyle = 'white';
+// ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = INITIAL_COLORS;
+ctx.fillStyle = INITIAL_COLORS;
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    x = event.offsetX;
+    y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onmouseDown(event) {
+    painting = true;
+}
+
+function handleCanvasClick() {
+    if (filling) ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.download = image;
+    link.click();
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onmouseDown);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    console.log(color);
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const rangeValue = event.target.value;
+    ctx.lineWidth = rangeValue;
+}
+
+function handleModeClick(event) {
+    if (filling === true) {
+        filling = false;
+        mode.innerText = 'Заливка';
+    } else {
+        filling = true;
+        mode.innerHTML = 'Рисование';
+    }
+
+}
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveClick);
+}
